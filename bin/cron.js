b512e9684a5675e950db29ede81bb0489822ba42
Austin Pray <austin@austinpray.com>
2016-07-27 18:05:36 -0500

    Cron

--- bin/cron.js
+++ bin/cron.js
@@ -1,1 +1,9 @@
-console.log("cron");
+console.log("Running cron");
+
+const redis = require("redis").createClient(process.env.REDISTOGO_URL);
+const {updateProductInfo} = require("../lib/microcenter");
+const scopedClient = require("scoped-http-client");
+
+const http = url => scopedClient.create(url);
+
+updateProductInfo({http, redis});
